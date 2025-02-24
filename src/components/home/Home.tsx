bd398297e0d92db1b55859e040427014dab64619
Michael Wood <mwood@data-crush.com>
2025-02-23 20:45:22 -0500

    example dynamic component

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,14 @@
+import { Page } from "../page/Page";
+
 export const Home: React.FC = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Page
+        name="div"
+        props={{ className: "cheesy-blink" }}
+        children={"welcome"}
+      />
     </div>
   );
 };
--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -1,37 +1,17 @@
 import React from "react";
-import { useLoaderData } from "react-router";
 
 // import { isValidElementType } from "react-is";
 // Explore using this for any html element: isValidElementType(elementName)
 // this would allow all html elements to be rendered instead of just a div
 
-type Config = Record<string, Record<string, any>>;
+// type Config = Record<string, Record<string, any>>;
 
-interface PageProps {
-  componentLoader: (
-    name: string,
-    props: Record<string, any>
-  ) => React.ReactElement | null;
-}
+type Config = {
+  name: string;
+  props: Record<string, any>;
+  children?: React.ReactNode | React.ReactNode[];
+};
 
-export const Page: React.FC<PageProps> = ({ componentLoader }) => {
-  const config = useLoaderData() as Config;
-
-  const renderElement = (elementName: string, props: Record<string, any>) => {
-    if (elementName === "div") {
-      return (
-        <div {...props}>{props.children && renderConfig(props.children)}</div>
-      );
-    }
-    return componentLoader(elementName, props);
-  };
-
-  const renderConfig = (config: Config) =>
-    Object.entries(config).map(([elementName, props], index) =>
-      React.createElement(() => renderElement(elementName, props), {
-        key: index,
-      })
-    );
-
-  return <>{renderConfig(config)}</>;
+export const Page: React.FC<Config> = (config) => {
+  return React.createElement(config.name, config.props, config?.children);
 };
